efecd2d5e5de87b5f22972c59f1676e1d8ab3d89
Tanishq Ranjan Verma <104843503+TanishqRAnjanVerma@users.noreply.github.com>
2023-10-20 23:52:25 +0530

    y

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -22,7 +22,40 @@ const Product = () => {
         <h1>BEST SELLER</h1>
       </div>
 
-      
+      <div
+        id="carouselExampleAutoplaying"
+        class="carousel slide"
+        data-bs-ride="carousel"
+        data-aos="fade-right"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active" style={{ objectFit: "cover" }}>
+            <img src={img32} class="d-block w-100" alt="..." />
+          </div>
+         
+          <div class="carousel-item">
+            <img src={img34} class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
 
       <div
         className="contain"
@@ -218,40 +251,7 @@ const Product = () => {
           </div>
         </div>
       </div>
-      <div
-        id="carouselExampleAutoplaying"
-        class="carousel slide"
-        data-bs-ride="carousel"
-        data-aos="fade-right"
-      >
-        <div class="carousel-inner">
-          <div class="carousel-item active" style={{ objectFit: "cover" }}>
-            <img src={img32} class="d-block w-100" alt="..." />
-          </div>
-         
-          <div class="carousel-item">
-            <img src={img34} class="d-block w-100" alt="..." />
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+
 
 
       <div className="slidee" data-aos="fade-up" >
